7051938e4b452778be6145a8609a3326153df4c7
Parthy <parththipan@web.de>
2018-03-19 20:57:31 +0100

    add CommonJS, umd, and es loader support

--- webpack.config.js
+++ webpack.config.js
@@ -4,42 +4,92 @@ const path = require('path');
 const pkg = require('./package.json');
 
 let libraryName = pkg.name;
-
+//
 let outputFile = libraryName + '.js';
+//
+// const config = {
+//     entry: './src/index.js',
+//     output: {
+//         path: __dirname + '/lib',
+//         filename: outputFile,
+//         library: libraryName,
+//         libraryTarget: 'umd',
+//         libraryExport: "default",
+//         umdNamedDefine: true
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /(\.js)$/,
+//                 loader: 'babel-loader',
+//                 exclude: /(node_modules)/
+//             }
+//         ],
+//     },
+//     optimization: {
+//         runtimeChunk: false,
+//         minimizer: [
+//             new UglifyJsPlugin({
+//                 cache: true,
+//                 parallel: true,
+//                 sourceMap: true
+//             })
+//         ]
+//     },
+//     resolve: {
+//         modules: [path.resolve('./node_modules'), path.resolve('./src')],
+//         extensions: ['.json', '.js']
+//     }
+// };
+//
+// module.exports = config;
 
-const config = {
-    entry: './src/index.js',
-    output: {
-        path: __dirname + '/lib',
-        filename: outputFile,
-        library: libraryName,
-        libraryTarget: 'umd',
-        libraryExport: "default",
-        umdNamedDefine: true
-    },
+
+const webpack = require('webpack');
+
+const { NODE_ENV } = process.env;
+
+const plugins = [
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+    }),
+];
+
+const filename = `${outputFile}${NODE_ENV === 'production' ? '.min' : ''}.js`;
+
+const minimizer = [];
+
+NODE_ENV === 'production' && minimizer.push(
+    new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+
+}));
+
+module.exports = {
     module: {
         rules: [
-            {
-                test: /(\.js)$/,
-                loader: 'babel-loader',
-                exclude: /(node_modules)/
-            }
+            { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ },
         ],
     },
-    optimization: {
-        runtimeChunk: false,
-        minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: true
-            })
-        ]
+
+    entry: [
+        './src/index',
+    ],
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename,
+        library: libraryName,
+        libraryTarget: 'umd',
     },
     resolve: {
         modules: [path.resolve('./node_modules'), path.resolve('./src')],
         extensions: ['.json', '.js']
-    }
+    },
+    optimization: {
+        runtimeChunk: false,
+        minimizer: minimizer
+    },
 };
-
-module.exports = config;
