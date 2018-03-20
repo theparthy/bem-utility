a8c1cd22254227ce61ec632fc24c8d16c1d8c89a
Parthy <parththipan@web.de>
2018-03-20 09:58:59 +0100

    Cleanup webpack config and Package json fix filename for umd build

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,20 @@
 /* global __dirname, require, module*/
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path');
-const pkg = require('./package.json');
-
-let libraryName = pkg.name;
-//
-let outputFile = libraryName + '.js';
-//
-// const config = {
-//     entry: './src/index.js',
-//     output: {
-//         path: __dirname + '/lib',
-//         filename: outputFile,
-//         library: libraryName,
-//         libraryTarget: 'umd',
-//         libraryExport: "default",
-//         umdNamedDefine: true
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /(\.js)$/,
-//                 loader: 'babel-loader',
-//                 exclude: /(node_modules)/
-//             }
-//         ],
-//     },
-//     optimization: {
-//         runtimeChunk: false,
-//         minimizer: [
-//             new UglifyJsPlugin({
-//                 cache: true,
-//                 parallel: true,
-//                 sourceMap: true
-//             })
-//         ]
-//     },
-//     resolve: {
-//         modules: [path.resolve('./node_modules'), path.resolve('./src')],
-//         extensions: ['.json', '.js']
-//     }
-// };
-//
-// module.exports = config;
-
-
 const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
+const pkg = require('./package.json');
+let libraryName = pkg.name;
 const { NODE_ENV } = process.env;
 
-const plugins = [
-    new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
-    }),
-];
-
-const filename = `${outputFile}${NODE_ENV === 'production' ? '.min' : ''}.js`;
-
+const filename = `${libraryName}${NODE_ENV === 'production' ? '.min' : ''}.js`;
 const minimizer = [];
 
 NODE_ENV === 'production' && minimizer.push(
     new UglifyJsPlugin({
         cache: true,
         parallel: true,
-        sourceMap: true
-
+        sourceMap: false
 }));
 
 module.exports = {
@@ -73,11 +23,9 @@ module.exports = {
             { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ },
         ],
     },
-
     entry: [
         './src/index',
     ],
-
     output: {
         path: path.join(__dirname, 'dist'),
         filename,
@@ -92,4 +40,9 @@ module.exports = {
         runtimeChunk: false,
         minimizer: minimizer
     },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+        })
+    ]
 };
